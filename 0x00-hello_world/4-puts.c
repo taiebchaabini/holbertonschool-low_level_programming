97d7ed3624cbca945f40b5ffe2c4d8d9467753af
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-18 20:01:53 +0000

    First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Printing something on the screen with PUTS */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
