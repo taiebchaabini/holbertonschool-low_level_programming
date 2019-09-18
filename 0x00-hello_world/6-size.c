1a7ff7994dcb6109acd66b97fef435cd7b7e650e
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-18 21:10:53 +0000

    First commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - A program that prints the size of various types on the computer it is compiled and run on
+  * main - Prints the size of various types it is compiled and run on
   * Return: Always 0 (Success)
   */
 int main(void)
