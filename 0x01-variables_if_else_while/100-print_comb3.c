3b32b693cc38b1869198a0868b28fc5ba15e6ef0
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-19 20:20:56 +0000

    Program fix

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible
+ * different combinations of two digits.
+ * Return: 0
+ **/
+
+int main(void)
+{
+	short int n, i;
+
+	n = 0;
+	i = 0;
+	while (i <= 9)
+	{
+	n = i + 1;
+	while (n <= 9)
+	{
+	putchar(i + '0');
+	putchar(n + '0');
+	if (i < 8 | n < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	n++;
+	}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
