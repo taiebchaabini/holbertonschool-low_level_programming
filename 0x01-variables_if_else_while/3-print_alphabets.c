b5fa06c0ca19c8994897e45b468912633c55dcde
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-19 16:20:23 +0000

    My first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase.
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int init_alphabet = 97;
+	int upper_alphabet = 65; 
+
+	while (init_alphabet <= 122)
+	{
+	putchar(init_alphabet);
+	init_alphabet++;
+	}
+	while (upper_alphabet <= 90)
+	{
+	putchar(upper_alphabet);
+	upper_alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
