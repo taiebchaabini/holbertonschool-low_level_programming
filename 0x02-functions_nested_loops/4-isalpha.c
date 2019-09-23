6c0066f1926694057e3ac7768abfc74147b82f29
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-23 22:50:57 +0000

    First commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * _isalpha - checks for alphabetic character.
+  * @c: parameter type int
+  * Return: 0 if the parameter is not a letter, lowercase or uppercase
+**/
+
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	return (1);
+	return (0);
+}
