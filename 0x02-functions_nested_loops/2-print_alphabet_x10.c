9c71448beae27502daef6020a9755a6ea6d4b12e
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-23 22:12:17 +0000

    First commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * main - Display 10 times alphabet
+  * Return: void
+**/
+
+void print_alphabet_x10(void)
+{
+	int a, b, i;
+
+	b = 'z';
+	for (i = 0; i < 10; i++)
+	{
+	for (a = 'a'; a <= b; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	}
+}
