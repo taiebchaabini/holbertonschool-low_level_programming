7296042d25503180423af0bb4ae9cf9d778b1cc3
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-24 02:15:01 +0000

    Fix to checking

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * main - Display Holberton with _putchar function
+  * Return: void
+**/
+
+int main(void)
+{
+	int a;
+	char text[] = "Holberton";
+
+	for (a = 0; a < 9; a++)
+	{
+	_putchar(text[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
