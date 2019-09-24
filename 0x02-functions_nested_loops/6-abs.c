10d713b88ea82b1e927f009ab547d31a9b82f333
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-24 02:30:31 +0000

    New commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * _abs - function that computes the absolute value of an integer.
+  * @a: parameter type int
+  * Return: if parameter is under 0 it make it positive
+**/
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	return (a * -1);
+	}
+	return (0);
+}
