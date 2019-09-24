46d60801d931e6facf72a81b297b302c4b034c5f
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-24 19:38:36 +0000

    Code fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,13 +29,10 @@ void print_to_98(int n)
 	for (i = n; i >= 98; i--)
 	{
 		printf("%d", i);
+		if (i != 98)
 		printf(", ");
 	}
-	for (i = n; i >= 98; i--)
-	{
-		printf("%d", i);
-		printf(", ");
-	}
+	printf("\n");
 	}
 	else if (n < 98)
 	{
