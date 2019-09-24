5ea97791f25bd53f4afd46a3da6d69f895023c9c
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-24 17:53:06 +0000

    First commit / Adding header

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+  * add - function that adds two integers and returns the result.
+  * @a: return int
+  * @b: return int
+  * Return: 0
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
