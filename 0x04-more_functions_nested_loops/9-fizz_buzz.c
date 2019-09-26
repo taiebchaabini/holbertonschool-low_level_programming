322ed4d25b3709591fabe6057314792470fc93a9
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-26 17:38:17 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * main - The “Fizz-Buzz test”
+  * Return: Always return 0
+ **/
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+	}
+	return (0);
+}
