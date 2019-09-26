b515b70187df18274ebc1a82c6ac7652e0b13b6f
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-26 14:02:19 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * print_most_numbers - function that prints the numbers, from 0 to 9.
+**/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
