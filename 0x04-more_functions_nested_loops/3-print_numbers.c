494e21a8f7078543df5cc63af2449e13114305f4
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-26 13:43:22 +0000

    Quick fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 
 /**
   * print_numbers - function that prints the numbers, from 0 to 9
-  * @a: type int
 **/
 void print_numbers(void)
 {
-	char a;
+	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a);
+		_putchar(a + '0');
 	}
 	_putchar('\n');
 }
