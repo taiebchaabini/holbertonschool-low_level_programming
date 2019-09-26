7d13861cc6fc4a5a3413b846166c4dcff9e0b6c7
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-09-26 14:18:54 +0000

    quick fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,12 @@ void more_numbers(void)
 	for (a = 0; a <= 14; a++)
 	{
 		c = a;
-		if(a > 9)
+		if (a > 9)
 		{
 			c = a / 10;
 		}
 		_putchar(c + '0');
-		if(a > 9)
+		if (a > 9)
 		{
 			_putchar(a % 10 + '0');
 		}
