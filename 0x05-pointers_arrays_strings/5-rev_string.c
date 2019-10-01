7497fe49e72efc0e54ac7aa7faae361cb9a9e2c8
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-01 19:27:47 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* rev_string - Function that reverse a string,
+* @s: pointer to char
+*/
+
+void rev_string(char *s)
+{
+	int n, i;
+	char temp;
+
+	n = 0;
+	i = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (n = n - 1; n >= 0; n--)
+	{
+		temp = s[i];
+		s[i] = s[n];
+		s[n] = temp;
+		i++;
+	}
+}
