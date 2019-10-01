4115f4ea5e288628ccce8a6a939b6418cec17872
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-01 19:37:47 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* puts2 - function that prints every other character of a string
+* @str: pointer to char
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
