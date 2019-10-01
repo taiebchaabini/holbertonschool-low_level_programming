f940e488a1fc2d92602a2e188682a0b646de36b5
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-01 19:57:41 +0000

    Betty is tricky

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_array - function that prints n elements of an array of integers, followed by a new line.
+* @str: pointer to char
+**/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
