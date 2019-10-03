4b3e6a201989d4539e2d9cb412ca696774c15277
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-03 14:50:27 +0000

    Fix to code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,15 @@ void reverse_array(int *a, int n)
 	int i;
 	char temp;
 
-	n = n - 1;
 	for (i = 0; i < n / 2; i++)
 	{
+		if (i == 0)
+			n = n - 1;
 		temp = a[i];
 		a[i] = a[n];
 		a[n] = temp;
 		n--;
 	}
+	for (i = i; i < n; i++)
+		a[i] = '\0';
 }
