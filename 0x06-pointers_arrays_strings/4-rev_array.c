6403e660b75ac30ef4e8a84c100ba241886a7ab7
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-03 20:53:01 +0000

    I tried like 4500 solutions

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,13 +5,13 @@
 * @a: pointer to char
 * @n: type int
 **/
-
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, b;
 	char temp;
 
-	for (i = 0; i < n / 2; i++)
+	b = n / 2;
+	for (i = 0; i < b; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - 1 - i];
