dc237d25143b1b55fd3135b73dc1d0fb0be9a19b
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 20:09:12 +0000

    Betty is tricky

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
 * _strstr - function that locates a substring.
 * @haystack: pointer to char
 * @needle: pointer to char
-* Return: 
+* Return:  Return the founded value
 **/
 
 char *_strstr(char *haystack, char *needle)
@@ -14,13 +14,13 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int b = 0;
 	int j = 0;
-	
+
 	while (haystack[l] != '\0')
 		l++;
 	while (needle[l2] != '\0')
 		l2++;
 
-        for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (b = 0; b < l2; b++)
 		{
@@ -29,7 +29,7 @@ char *_strstr(char *haystack, char *needle)
 			else
 				j = 0;
 		}
-		
+
 		if (j == l2)
 		{
 			haystack = haystack + i;
