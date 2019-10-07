f5b7de71dad6634b5ae237582aa7e27a8eac565e
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 11:42:19 +0000

    fix to code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,18 @@
 * @s: pointer to char - src table
 * @c: type char - character to found
 * Return: the first occurence if character is found
-* otherwise it returns null
+* otherwise it returns NULL also if the first character '\0'
 **/
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if (c == '\0')
-		return (0);
 	while (s[i] != '\0')
 	{
+		if (s[i] == '\0')
+			return (0);
+
 		if (s[i] == c)
 		{
 			s = s + i;
@@ -23,6 +24,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	
 	return (0);
 }
