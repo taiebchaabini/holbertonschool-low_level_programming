3e1179742d46bf7660d064f391e3ca119bec2311
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 11:09:01 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* _strchr - function that locates a character in a string.
+* @s: pointer to char - src table
+* @c: type char - character to found
+* Return: the first occurence if character is found
+* otherwise it returns null
+**/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int b = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c || b == 1)
+		{
+			s++;
+			b = 1;
+		}
+		i++;
+	}
+	if (b == 0)
+		return (0);
+	return (s);
+}
