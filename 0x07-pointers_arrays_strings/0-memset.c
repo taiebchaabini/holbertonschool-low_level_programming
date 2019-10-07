4c12a70e612cc5b7ba29d1f9a536fecf46be6102
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 10:20:20 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/** 
+* _memset - function that fills memory with a constant byte.
+* @s: pointer to char getting buffer table
+* @b: type char - new adress
+* @n: type unsigned int - space needed
+**/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return s;
+}
