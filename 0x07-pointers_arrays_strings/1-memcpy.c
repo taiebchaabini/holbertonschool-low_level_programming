27dcc4426842feeac24e257cbcfbdf13d5149fec
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 11:13:12 +0000

    fix to code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i = 0;
 	int j = 0;
 
-	for (; j < src[j] && i < n; i++)
+	for (; j <= src[j] && i < n; i++)
 	{
 	dest[i] = src[i];
 	j++;
