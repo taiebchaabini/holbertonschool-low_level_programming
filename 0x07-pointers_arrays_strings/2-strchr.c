e0ded4b15c0d4827ab9d9890f0d12d6ac6f803fa
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-07 11:46:28 +0000

    Fix to code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
+	if (c == '\0')
+		return (s);
 	while (s[i] != '\0')
 	{
-		if (s[i] == '\0')
-			return (0);
-
 		if (s[i] == c)
 		{
 			s = s + i;
