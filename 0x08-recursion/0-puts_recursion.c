3c81839bc3405189330ac1f6c509ccaaeed58c51
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-09 15:36:39 +0000

    First commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - Function that prints a string, follow by newline
+* @s: 
+**/
+
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
