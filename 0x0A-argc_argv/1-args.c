822655c2756e59b9ad145400e7e3586496d2380f
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-13 16:32:35 +0000

    First commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - a function that prints its name follow by a new line
+* @argc: count number of argument passed to the function size @argv
+* @argv: array of arguments passed size of @argc
+* Return: always return 0
+**/
+
+int main(int argc, char **argv)
+{
+	(void) *argv;
+	printf("%d\n", argc);
+	return (0);
+}
