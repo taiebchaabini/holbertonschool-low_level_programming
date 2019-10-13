d1115da623439c4b6f93e56ed43d33f75e542e02
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-13 16:44:03 +0000

    Betty is tricky

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-* main - Program that prints all arguments it receives. 
+* main - Program that prints all arguments it receives.
 * @argc: count number of argument passed to the function size @argv
 * @argv: array of arguments passed size of @argc
 * Return: always return 0
@@ -11,7 +11,7 @@
 int main(int argc, char **argv)
 {
 	int i = 0;
-	argc = argc;
+
 	for (i = i; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
