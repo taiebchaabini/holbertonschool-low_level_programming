03bd763ec3d84dcb0e5797e8e25a02c8bd604b81
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-13 16:42:23 +0000

    First commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Program that prints all arguments it receives. 
+* @argc: count number of argument passed to the function size @argv
+* @argv: array of arguments passed size of @argc
+* Return: always return 0
+**/
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	argc = argc;
+	for (i = i; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
