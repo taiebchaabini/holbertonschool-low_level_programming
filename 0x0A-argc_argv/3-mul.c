aa03b9818306d9602d32f35947bef6bd61c6a9c1
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-13 17:00:33 +0000

    First commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program that multiplies two numbers.
+* @argc: count number of argument passed to the function size @argv
+* @argv: array of arguments passed size of @argc
+* Return: always return 0
+**/
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	argc = argc - 1;
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	return (0);
+}
