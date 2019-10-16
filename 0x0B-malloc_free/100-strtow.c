3c23a2785fa825cd3d573a81edd97247261d538c
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-16 22:55:15 +0000

    Fix

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -3,53 +3,60 @@
 #include <stdio.h>
 
 /**
-* strtow - function that splits a string into words.
-* @str: h
-* @Return: h 
+* split_word - function that count the number of words
+* @str: Getting a string
+* Return: Number of words founds in a string delimited by white space
+
+/** strtow - function that splits a string into words.
+* @str: Getting a string
+* @Return: Array of words, without white space
 **/
 
-char **strtow(char *str)
-{
-	int i = 0, j = 0, b = 0, c = 0, r = 0;
-	int chars = 0, words = 0;
-	char **p;
 
-	if (str == NULL)
-		return (NULL);
+int split_word(char *str)
+{
+	int i = 0, words = 0;
 
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ' && str[i - 1] == ' ')
 			words++;
-		if (str[i] != ' ')
-			chars++;
 		i++;
 	}
+	return (words);
+}
 
+
+char **strtow(char *str)
+{
+	int i = 0, j = 0, b = 0, c = 0, r = 0;
+	int words = 0;
+	char **p;
+
+	if (str == NULL)
+		return (NULL);
+	words = split_word(str);
 	p = malloc(words * sizeof(char *));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	i = 0;
-	c = 0;
+	i = 0, c = 0;
 	for (j = 0; j < words; j++)
 	{
 		b = 0, r = 0;
 		for (; str[i] == ' '; i++)
 			;
-		for(; str[i] != ' '; i++)
-		{
+		for (; str[i] != ' '; i++)
 			b++;
-		}
 		b = b + 1;
 		p[j] = malloc(b * sizeof(char));
 		if (p[j] == NULL)
 		{
 			for (; j >= 0; j--)
 				free(p[j]);
-			free(p);	
+			free(p);
 		}
 		for (; str[c] == ' '; c++)
 			;
