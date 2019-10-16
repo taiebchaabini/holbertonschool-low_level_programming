fe09723d4f6bfbe17cd7f762fc974ab6ecda4c46
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-16 13:55:05 +0000

    Fix code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,8 +18,13 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	else
+	{
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
 	*(p + i) = '\0';
+	}
 	return (p);
 }
