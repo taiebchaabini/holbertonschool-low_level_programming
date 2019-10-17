0fb2a20f5d6aae8dd263e24309b6698faab39bba
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-17 18:40:19 +0000

    Fix code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include "string.h"
 #include <stdlib.h>
 
 /**
@@ -11,13 +10,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	void *p = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	memset(p, ' ', 0);
 	return (p);
 }
