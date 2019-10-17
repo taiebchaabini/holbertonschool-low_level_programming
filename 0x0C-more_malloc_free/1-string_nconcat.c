f0e02f7a9ae3159c798596159c1c98887aab6091
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-17 16:28:15 +0000

    Betty is tricky

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,21 +3,22 @@
 
 /**
 * string_nconcat - function that concatenates two strings.
-* @s1:
-* @s2:
-* n: 
+* @s1: Taking first string
+* @s2: Taking second string
+* @n: limit size of concatenates string of @s2
+* Return: concatenated string
  **/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned i = 0, b = 0, d = 0, c = 0;
+	unsigned int i = 0, b = 0, d = 0, c = 0;
 	char *p;
 
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 		i++;
 	if (s2 != NULL)
 	{
-	while(s2[b] != '\0' || b < n)
+	while (s2[b] != '\0' || b < n)
 		b++;
 	}
 	p = malloc(i + b * sizeof(char));
