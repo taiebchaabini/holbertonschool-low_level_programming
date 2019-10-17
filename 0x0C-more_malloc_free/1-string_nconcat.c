b410792085e5e151d8c9eeb043a4cfdee7352b99
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-17 17:22:47 +0000

    Fix code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[b] != '\0' || b < n)
 		b++;
 	}
-	p = malloc(i + n * sizeof(char));
+	p = malloc(i + b + 1 * sizeof(char));
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
+	}
 	for (d = 0; d < i; d++)
 	{
 		p[d] = s1[c];
