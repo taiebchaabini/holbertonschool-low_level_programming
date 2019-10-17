a196e4531bda5581514024c15cd891172cae0953
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-17 18:11:34 +0000

    First commit / Fixing header

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _calloc - function that allocates memory for an array, using malloc.
+* @nmemb: number of elements
+* @size: size of type
+* Return: pointer
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	return (p);
+}
