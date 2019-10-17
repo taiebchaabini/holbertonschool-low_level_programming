44aad2cc6b346aeac5488e260fed0cb2ae75598c
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-17 18:58:07 +0000

    Fix code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = 0;
+	char *p;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	while(i < nmemb * size)
+	{
+		p[i] = 0;
+		i++;
+	}
 	return (p);
 }
