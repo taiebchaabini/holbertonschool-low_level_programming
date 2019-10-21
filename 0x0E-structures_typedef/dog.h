e933016196bcc9477d5a5372fc86ce434db6f614
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-21 14:59:04 +0000

    fix code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
