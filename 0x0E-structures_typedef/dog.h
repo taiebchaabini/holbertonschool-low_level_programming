25739999f027fb94db851026f197e715d0977554
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-21 14:00:14 +0000

    Fix code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOGHEADER
 #define DOGHEADER
+/**
+* struct dog - structure for dogs
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+**/
 struct dog
 {
 	char *name;
