235f69e5892d6de0593d0959dbc43ccb10868695
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-21 16:19:49 +0000

    GOOO work

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,10 +35,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
+	for (i = 0; i <= len1; i++)
 		dog->name[i] = name[i];
 	dog->age = age;
-	for (i = 0; owner[i] != '\0'; i++)
+	for (i = 0; i <= len2; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
