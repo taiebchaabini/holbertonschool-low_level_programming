334cb48a51537662873e3114d1d72791211081e7
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-21 15:12:15 +0000

    First commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog - function that create a new dog
+* @name: name of the new dog
+* @age: age of the new dog
+* @owner: owner of the new dog
+* Return: return pointer to struct
+**/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
