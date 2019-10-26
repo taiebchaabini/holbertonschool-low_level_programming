1954a68e2b5df3d3892aa6178e7a31fca49a5978
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-26 12:09:23 +0000

    Fix code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		p = va_arg(list, char*);
-		if (p[0] == '\0')
+		if (p == NULL)
 			printf("(nil)");
 		else
 			printf("%s", p);
