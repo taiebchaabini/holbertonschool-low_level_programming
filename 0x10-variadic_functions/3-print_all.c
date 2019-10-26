4198cdf6290f327be85922638b261c87b6bba27f
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-10-26 16:43:20 +0000

    Fix code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,5 @@
 #include "variadic_functions.h"
-/**
-* _printc - function that print a char
-* @list: take the list
-**/
-void _printc(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-/**
-* _printi - function that print an integer
-* @list: take the list
-**/
-void _printi(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-/**
-* _printf - function that print a float
-* @list: take the list
-**/
-void _printf(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
-* _prints - function that print a string
-* @list: take the list
-**/
-void _prints(va_list list)
-{
-	char *p;
-
-	p = va_arg(list, char*);
-	if (p == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", p);
-}
+#include "my_functions.h"
 /**
  * print_all - function that prints anything.
  * @format: format of the passed arguments
@@ -46,8 +7,8 @@ void _prints(va_list list)
 void print_all(const char * const format, ...)
 {
 	int i = 0, b = 0;
-	va_list list;
 	void (*fp)(va_list);
+	va_list list;
 
 	type t[] = {
 		{"c", _printc},
