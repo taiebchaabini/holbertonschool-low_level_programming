620fb2cf80638536e03bfbdc89dfcb3ed9581285
Taieb CHaabini <taiebchaabini@gmail.com>
2019-10-31 20:35:59 +0000

    First commit and i hope it check

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* list_len -  function that returns the number of elements in a linked list_t list.
+* @h: take a list of struct node
+* Return: number of elements into the node list
+**/
+size_t list_len(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int count = 0;
+
+	tmp = NULL;
+	tmp = h;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
