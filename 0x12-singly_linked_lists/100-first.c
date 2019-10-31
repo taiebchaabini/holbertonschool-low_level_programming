ab6c13f38c783fc3411447b690872d8a932a8535
Taieb CHaabini <taiebchaabini@gmail.com>
2019-10-31 23:29:38 +0000

    Fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,6 @@
 **/
 void my_function(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
