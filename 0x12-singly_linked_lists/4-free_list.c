8184ee48216265f47c68597c528be0bff30c44c3
Taieb CHaabini <taiebchaabini@gmail.com>
2019-10-31 22:56:57 +0000

    First commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+* free_list - function that frees a list_t list.
+* @head: pointer to head of list_t
+**/
+void free_list(list_t *head)
+{
+	while (head->next != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
