88ba26b488bf28411e57e031ce84f6902a34fa20
Taieb CHaabini <taiebchaabini@gmail.com>
2019-10-31 21:09:49 +0000

    Fix code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-* list_len -  function that returns the number of elements in a linked list_t list.
+* list_len -  function that returns the number of elements in
+* a linked list_t list.
 * @h: take a list of struct node
 * Return: number of elements into the node list
 **/
