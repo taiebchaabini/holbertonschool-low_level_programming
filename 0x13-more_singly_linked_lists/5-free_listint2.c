5a60daa8639841086e4aad44f6bb6ecaa98d0fb4
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-04 10:43:38 +0000

    First commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+/**
+ * free_listint2 -  function that frees a listint_t list.
+ * @head: pointer of list type of listint_t
+ **/
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *h;
+
+	h = *head;
+	while (h)
+	{
+		tmp = h->next;
+		free(h);
+		h = tmp;
+	}
+}
