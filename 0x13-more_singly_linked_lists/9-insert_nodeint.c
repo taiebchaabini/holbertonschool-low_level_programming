f6693f17e8acfa18b21b64af6b2f3c29e9299433
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-05 10:59:00 +0000

    Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tmp = h->next;
 			new->next = tmp;
 			h->next = new;
-			if (!h->next)
+			if (!(h->next))
 			{
 				free(new);
 				return (NULL);
