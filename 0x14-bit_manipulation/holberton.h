a856ed778b66f0a6d59b6913257b1f53357f8dca
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-07 22:32:36 +0000

    Fix headers

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -3,6 +3,6 @@
 #include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-void tc_rest(unsigned long int n)
+void tc_rest(unsigned long int n);
 int _putchar(char c);
 #endif
