8f4de00cccf665b4c02564cb28a0d8d2c5268ebc
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-07 22:31:57 +0000

    Fix code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
 /**
+* tc_rest - function that convert deciam to binary
+* @n: take a decimal number as argument
+**/
+void tc_rest(unsigned long int n)
+{
+	if (n != 0)
+		tc_rest(n >> 1);
+	if (n & 1)
+		_putchar(1 + '0');
+	else if (!(n & 0))
+		_putchar(0 + '0');
+}
+/**
  * print_binary - function that prints the binary representation of a number.
  * @n: take a decimal number as argument
  **/
 void print_binary(unsigned long int n)
 {
-	if (n != 0)
-		rest(n >> 1);
-	if (n & 1)
-		_putchar(1 + '0');
-	if (!(n & 1))
-		_putchar(0 + '0');
+	tc_rest(n);
 }
