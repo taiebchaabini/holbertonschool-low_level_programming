a49d57256ccbab7d60dec6dd483f82254585f851
Taieb CHaabini <taiebchaabini@gmail.com>
2019-11-12 22:08:35 +0000

    Fix code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
-* c_file - copy the content from file 1 to new file2
-* or existing file
-* @file1: src file
-* @file2: dest file
-**/
+ * c_file - copy the content from file 1 to new file2
+ * or existing file
+ * @file1: src file
+ * @file2: dest file
+ **/
 void c_file(char *file1, char *file2)
 {
 	int i = 0, fd = 0, fd2 = 0, wstate = 0, cstate = 0, cstate2 = 0;
@@ -21,12 +21,13 @@ void c_file(char *file1, char *file2)
 	}
 	while ((i = read(fd, &buf, 1024)) != 0)
 	{
-		wstate = write(fd2, &buf, i);
 		if (wstate == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
-		exit(99);
+			dprintf(stderr_fileno, "error: can't write to %s\n", file2);
+			exit(99);
 		}
+
+		wstate = write(fd2, &buf, i);
 	}
 
 	cstate = close(fd);
@@ -38,11 +39,11 @@ void c_file(char *file1, char *file2)
 
 }
 /**
-* main - program that copies the content of a file to another file.
-* @ac: number of arguments
-* @av: arguments of the program
-* Return: 0 if success otherwise exit with ERROR CODE
-**/
+ * main - program that copies the content of a file to another file.
+ * @ac: number of arguments
+ * @av: arguments of the program
+ * Return: 0 if success otherwise exit with ERROR CODE
+ **/
 int main(int ac, char **av)
 {
 	if (ac != 3)
