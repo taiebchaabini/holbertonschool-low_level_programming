c310ea538789c8d93e2c098bad40511c8e101d5d
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-12 19:24:40 +0000

    Fix code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
 **/
 int main(int ac, char **av)
 {
-	int fd = 0, i = 0, fd2 = 0, wstate = 0, FD_VALUE = 0;
+	int fd = 0, i = 0, fd2 = 0, wstate = 0;
 	char *buf;
 
 	if (ac != 3)
@@ -30,18 +30,16 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 664);
 	wstate = write(fd2, buf, i);
 	if (fd2 == -1 || wstate == -1 || wstate != i)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	FD_VALUE = close(fd);
-	close(fd2);
-	if (FD_VALUE == -1)
+	if (close(fd) == -1 || close(fd2) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FD_VALUE);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", -1);
 		exit(100);
 	}
 	return (0);
