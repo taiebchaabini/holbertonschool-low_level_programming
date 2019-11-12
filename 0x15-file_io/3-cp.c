13d320b457e2ea6248e8e6aec0dbb12c6420a5cd
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-12 19:34:55 +0000

    Fix code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
 **/
 int main(int ac, char **av)
 {
-	int fd = 0, i = 0, fd2 = 0, wstate = 0;
+	int fd = 0, i = 0, fd2 = 0, wstate = 0, cstate = 0, cstate2 = 0;
 	char *buf;
 
 	if (ac != 3)
@@ -19,25 +19,22 @@ int main(int ac, char **av)
 	if (buf == NULL)
 		exit(-1);
 	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
-		exit(98);
-	}
 	i = read(fd, buf, 1024);
-	if (i == -1)
+	if (i == -1 || fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 664);
+	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 00664);
 	wstate = write(fd2, buf, i);
 	if (fd2 == -1 || wstate == -1 || wstate != i)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	if (close(fd) == -1 || close(fd2) == -1)
+	cstate = close(fd);
+	cstate2 = close(fd2);
+	if (cstate == -1 || cstate2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", -1);
 		exit(100);
