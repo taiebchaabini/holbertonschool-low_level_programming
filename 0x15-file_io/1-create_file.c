8d74747dd2bb23213a44332d8fd49e0fd2d8c932
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-12 16:02:15 +0000

    Fix code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i] != '\0')
 		i++;
 	wstate = write(fd, text_content, i);
-	if (wstate == -1 || wstate != i)
+	if (wstate == -1)
 	{
 		write(1, error, 6);
 		return (-1);
