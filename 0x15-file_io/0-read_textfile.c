71c905ed1104e329ff9990c7af148d9a1668dba9
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-12 13:55:39 +0000

    First commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* read_textfile - function that reads a text file and prints it to the POSIX
+* standard output.
+* @filename: file path to open
+* @letters: number of letter to read and print to standard ouput
+* Return: if error 0 otherwise number of characters readed from
+* the file and should be printed
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0, i = 0;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters + 1);
+
+	if (buf == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	read(fd, buf, letters);
+	buf[letters] = '\0';
+
+	while (buf[i] != '\0')
+		i++;
+
+	if (write(STDOUT_FILENO, buf, letters) == -1)
+		return (0);
+
+	close(fd);
+	return (i);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef holberton_h
+#define holberton_h
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
