213cc4dd3988f3cb0d048a1314b3d0825dfec7d4
Taieb CHaabini <taiebchaabini@gmail.com>
2019-11-13 08:07:14 +0000

    Fix to code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ void c_file(char *file1, char *file2)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
 		exit(99);
 	}
-	while ((i = read(fd, buf, 1024)) != 0)
+	while ((i = read(fd, buf, 1024)) > 0)
 	{
 		wstate = write(fd2, buf, i);
 		if (wstate == -1)
