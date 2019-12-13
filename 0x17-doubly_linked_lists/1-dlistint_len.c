72d4e9d43a5c79cb21421c519c4376ad6f665106
Taieb CHaabini <taiebchaabini@gmail.com>
2019-12-13 10:18:43 +0000

    Betty is cool

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 /**
-* print_dlistint - function that returns the number of elements in a linked dlistint_t list.
+* dlistint_len - function that returns the number of elements in a
+* linked dlistint_t list.
 * @h: dlistint_t header pointer
 * Return: number of nodes
 **/
 size_t dlistint_len(const dlistint_t *h)
 {
 	int i = 0;
+
 	while (h)
 	{
 		i++;
