2f593b0a1fa700eae779773bc7d179015f631042
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-14 20:36:48 +0000

    fixes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,6 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = tmp;
 			new->next = tmp->next;
 			tmp->next = new;
+			tmp = tmp->next;
+			tmp->prev = new;
 		}
 		i++;
 		tmp = tmp->next;
