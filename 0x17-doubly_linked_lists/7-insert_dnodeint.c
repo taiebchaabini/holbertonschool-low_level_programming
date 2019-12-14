153cf78ec710a3c214781d12ebfed1c7bb1d8da8
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-14 21:24:25 +0000

    Fixes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,11 +24,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
+	if (idx == len)
+		return(add_dnodeint_end(h, n));
 	if (*h == NULL)
-	{
-		*h = new;
-		return (new);
-	}
+		return(add_dnodeint(h, n));
 	if (idx == 0)
 	{
 		new->next = tmp;
