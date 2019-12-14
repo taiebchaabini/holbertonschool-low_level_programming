e7de4fe9fbdf25ed7a702abc1bba3f745ff3f5d3
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-14 20:58:32 +0000

    Updates

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0, len = 0;
 	dlistint_t *new = NULL, *tmp = NULL;
 
+	if (!h)
+		return (NULL);
 	tmp = *h;
 	for (len = 0; tmp; len++, tmp = tmp->next)
 		;
