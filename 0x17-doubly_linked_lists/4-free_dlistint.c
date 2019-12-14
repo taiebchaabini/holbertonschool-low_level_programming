94c8cd8ba3c5f7be36aaf9f0f73a7e4ed30f8cff
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-14 22:59:19 +0000

    Fixes

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,7 +7,10 @@ void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
 		return;
-	for (; head; head = head->next)
+	while (head->next)
+	{
+		head = head->next;
 		free(head->prev);
+	}
 	free(head);
 }
