48962f29b86951cf790ef50c20d17011d56a1764
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-12-15 01:58:43 +0000

    Fixes

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,6 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		old = tmp;
+		tmp->next->prev = NULL;
 		*head = tmp->next;
 		free(tmp);
 		return (1);
