db1e58794453a7ceca91c89ec4806d853c2b631e
taiebchaabini <taiebchaabini@gmail.com>
2020-01-19 20:49:29 +0000

    Fix to malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = malloc(sizeof(hash_node_t) * size / 3);
+	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (new->array == NULL)
 		return (NULL);
 	return (new);
