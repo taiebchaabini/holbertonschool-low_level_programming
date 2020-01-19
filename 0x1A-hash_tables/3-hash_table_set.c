854216d9f08a54170fa37b179d62ef3f46f1b3d8
taiebchaabini <taiebchaabini@gmail.com>
2020-01-19 11:55:47 +0000

    first commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dval = "";
 	unsigned long int index = 0;
 
-	if (strlen(key) == 0 || ht->array == NULL)
+	if (strlen(key) == 0 || ht == NULL || ht->array == NULL)
 		return (0);
 
 	head = ht->array;
