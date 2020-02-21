4ef42fd9994931d22649bb1276a88f9408d8e546
taiebchaabini <taiebchaabini@gmail.com>
2020-02-21 22:51:19 +0000

    Handling first position and last position

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -33,14 +33,22 @@ def island_perimeter(grid):
     perimeter = 0
     lasty = 0
     lastx = 0
+    positions = []
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 1:
                 lastx = x
                 lasty = y
-                if x == lastx and grid[y - 1][x] == 1 or grid[y + 1][x] == 1:
-                    height += 1
-                if y == lasty and grid[y][x - 1] == 1 or grid[y][x + 1] == 1:
-                    width += 1
+                try:
+                    if y > 0 and grid[y - 1][x] == 1 or y < len(grid) - 1\
+                            and grid[y + 1][x] == 1:
+                        positions.append(str(lasty) + ":" + str(lastx))
+                        height += 1
+                    if x > 0 and grid[y][x - 1] == 1 or y < len(grid[y]) - 1\
+                            and grid[y][x + 1] == 1:
+                        positions.append(str(lasty) + ":" + str(lastx))
+                        width += 1
+                except Exception as e:
+                    raise e
     perimeter = (width + height) * 2
     return (perimeter)
