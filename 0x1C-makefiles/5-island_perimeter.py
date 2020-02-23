dd70b996226295ff09b2217f0a4e01ba74b1c67c
taiebchaabini <taiebchaabini@gmail.com>
2020-02-23 20:58:55 +0000

    New algo

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -28,11 +28,41 @@ def island_perimeter(grid):
     * The island doesn’t have “lakes” (water inside that isn’t connected to the
     water around the island).
     """
+    height = 0
+    width = 0
+    maxw = []
+    maxh = []
     perimeter = 0
-    for y in grid:
-        for x in y:
-            if x == 1:
-                perimeter += 4
-    if perimeter > 0:
-        perimeter = int((perimeter / 2) + 2)
+    lasty = 0
+    lastx = 0
+    positions = []
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == 1:
+                lastx = x
+                lasty = y
+                try:
+                    pos = str(lasty) + ":" + str(lastx)
+                    if y > 0 and grid[y - 1][x] == 1 or y < len(grid) - 1\
+                            and grid[y + 1][x] == 1:
+                            if pos not in positions:
+                                height += 1
+                                positions.append(pos)
+                    if x > 0 and grid[y][x - 1] == 1 or y < len(grid[y]) - 1\
+                            and grid[y][x + 1] == 1:
+                            if pos not in positions:
+                                positions.append(pos)
+                                width += 1
+                    if height > maxh:
+                        maxh = height
+                    if width > maxw:
+                        maxw = width
+                except Exception as e:
+                    pass
+    for i in positions:
+        if i[0] not in maxh:
+            maxh += i[0]
+        if i[2] not in maxw:
+            maxw += i[2]
+    perimeter = (len(maxw) + len(maxh)) * 2
     return (perimeter)
