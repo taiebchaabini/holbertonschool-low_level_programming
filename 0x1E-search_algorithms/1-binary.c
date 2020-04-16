a00ecf7b4476e1df0f59e2c75ab8f190fabfa85b
taiebchaabini <taiebchaabini@gmail.com>
2020-04-16 13:05:26 +0000

    New algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,11 +32,10 @@ int binary_search(int *array, size_t size, int value)
 		}
 		if (array[mid] == value)
 			return (mid);
-
-		if (array[mid] < value)
-			left = mid + 1;
 		else if (array[mid] > value)
 			right = mid - 1;
+		else if (array[mid] <= value)
+			left = mid + 1;
 	}
 	return (-1);
 }
